68ab85d22ca550407d679c34e549022d44a756cd
Hom Dahal <homdahal997@gmail.com>
2024-06-03 18:08:47 -0400

    Renamed users related files to make name more clear based on functions they perform

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const usersCtrl = require('../controllers/users.js');
+const usersCtrl = require('../controllers/usersController.js');
 
 const router = Router();
 
